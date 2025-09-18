691694158fb116241e72c1b9a97d7507f606a81d
kmiiyaaa <ckm1533@naver.com>
2025-09-18 15:55:09 +0900

    first commit

---
+++ js/example.js
@@ -0,0 +1,9 @@
+document.write("Hello!");
+document.write('Hello "안녕하세요" World');
+
+var str1 = "kor";
+var str2 = "jap";
+var num1 = 100;
+var num2 = Number("100"); // 문자100 -> 숫자100
+var tag1 = "<h1> Strung </h1>";
+var bool1 = Boolean(null); // false -> 0과 null이 아닌값은 모두 true (음수도 true)
